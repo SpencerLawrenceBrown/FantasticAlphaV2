d89bef6fdc68661a672f739214f720653b3defd1
Spencer Lawrence Brown <spencrbrown@gmail.com>
2015-04-15 01:13:59 -0700

    fixed master back to alpha master"

--- config/database.js
+++ config/database.js
@@ -1,6 +1,8 @@
 //Database access
 module.exports = {
 	//'url' : 'mongodb://127.0.0.1/fantastic-alpha-v2'
-	'url' : 'mongodb://fantasticcrew:#Ellendale2813@proximus.modulusmongo.net:27017/uwun3uhE'
+	//Sample 'url' : 'mongodb://fantasticcrew:#Ellendale2813@proximus.modulusmongo.net:27017/uwun3uhE'
+	//Alpha 
+	'url' : 'mongodb://fantasticcrew:#Ellendale2813@proximus.modulusmongo.net:27017/ywymyS9y'
 };
 
