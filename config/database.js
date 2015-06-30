3302df0d316113646ebe1dcc96b2e689d3a9d2a0
Spencer Lawrence Brown <spencrbrown@gmail.com>
2015-06-29 19:59:37 -0700

    actual final with updated campaign

--- config/database.js
+++ config/database.js
@@ -3,6 +3,6 @@ module.exports = {
 	//'url' : 'mongodb://127.0.0.1/fantastic-alpha-v2'
 	//Sample 'url' : 'mongodb://fantasticcrew:#Ellendale2813@proximus.modulusmongo.net:27017/uwun3uhE'
 	//Alpha 5588a0bdb9d207bc5619fada
-	'url' : 'mongodb://fantasticcrew:#Ellendale2813@apollo.modulusmongo.net:27017/Eg8obura'
+	'url' : 'mongodb://fantasticcrew:#Ellendale2813@apollo.modulusmongo.net:27017/eZi8vuhu'
 };
 
